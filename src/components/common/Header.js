0228f6ced974d85ebef7c58d9a171eeccf9e546b
Lima <T_M_Lima@DellTeam.com>
2020-11-26 15:42:49 -0300

    courses page added

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -8,6 +8,8 @@ const Header = () => {
         <nav>
             <NavLink to="/" activeStyle={activeStyle} exact>Home</NavLink>
             {" | "}
+            <NavLink to="/courses" activeStyle={activeStyle} exact>Courses</NavLink>
+            {" | "}
             <NavLink to="/about" activeStyle={activeStyle} exact>About</NavLink>
         </nav>
     );
